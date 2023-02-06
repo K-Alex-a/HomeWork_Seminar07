5037b82706628463e32720ed584e0f7024910e8d
Alex_a <shiotoy@icloud.com>
2023-02-06 21:24:10 +0300

    in process

--- task047/Program.cs
+++ task047/Program.cs
@@ -1,1 +1,77 @@
-﻿
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Вариант 1 (через ввод размера пользователем):
+Console.Write("Введите кол-во строк (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = new double[m, n];
+Random rnd = new Random();
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round((rnd.NextDouble() * 100 - 50), 1);
+        }
+    }
+}
+
+PrintArr(arr);
+
+
+
+void PrintArr(double[,] arry)  // метод вывода на печать массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j],8}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+// Вариант 2 (полный рандом):
+// Random rnd = new Random();
+// int m = rnd.Next(1, 20);
+// Console.WriteLine($"Строк будет: {m}");
+// int n = rnd.Next(1, 10);
+// Console.WriteLine($"Столбцов будет: {n}");
+
+// double[,] arr = new double[m, n];
+
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = Math.Round((rnd.NextDouble() * 100 - 50 ), 1);
+//         }
+//     }
+// }
+
+// PrintArr(arr);
+
+
+
+// void PrintArr(double[,] arry)  // метод вывода на печать массива
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arry[i, j],8}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
--- task052/Program.cs
+++ task052/Program.cs
@@ -5,3 +5,53 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+Random rnd = new Random();
+int m = rnd.Next(1, 5);
+Console.WriteLine($"Строк будет: {m}");
+int n = rnd.Next(1, 10);
+Console.WriteLine($"Столбцов будет: {n}");
+
+double[,] arr = new double[m, n];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round((rnd.NextDouble() * 100 ), 0);
+        }
+    }
+}
+
+PrintArr(arr);
+
+
+
+void PrintArr(double[,] arry)  // метод вывода на печать массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j],8}");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine();
+
+
+for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {double sum=0;
+            if (i  == 0 )
+            {
+                sum =arr[i, j] + arr[i+1, j]+ arr[i+2, j];
+            }Console.WriteLine(sum);
+        }
+    }
