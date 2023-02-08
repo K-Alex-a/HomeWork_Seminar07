4e60841895b043f6ec974feab4eb8cecc78e8251
Alex_a <shiotoy@icloud.com>
2023-02-09 00:04:14 +0300

    finish all

--- task050/Program.cs
+++ task050/Program.cs
@@ -6,4 +6,105 @@
 // 8 4 2 4
 // 1 7 -> такого числа в массиве нет
 // 1 1 -> 9
+// *попробовать через Split
 
+// Вариант 1:
+Random rnd = new Random();
+Console.WriteLine("Например, задан массив:");
+int[,] arr = new int[rnd.Next(3, 10), rnd.Next(5, 10)];
+Console.WriteLine($"- с количеством строк: {arr.GetLength(0)}");
+Console.WriteLine($"- с количеством столбцов: {arr.GetLength(1)}");
+FillArrayRandom(arr);
+PrintArr(arr);
+Console.Write("Введите номер строки: ");
+int k = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите номер столбца: ");
+int l = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+if (k < 0 | k >= arr.GetLength(0) | l < 0 | l >= arr.GetLength(1))
+{
+    Console.WriteLine($"arr[{k}, {l}] -> такого значения нет!");
+}
+else
+{
+    Console.WriteLine($"arr[{k}, {l}] -> {arr[k, l]}");
+}
+
+
+void FillArrayRandom(int[,] arr)    // создаем рандомный двумерный массив из целых чисел
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(0, 10);
+
+        }
+
+    }
+}
+
+void PrintArr(int[,] arry)  // метод вывода на печать массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j],2}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// Вариант 2(*через Split):
+
+// Random rnd = new Random();
+// Console.WriteLine("Например, задан массив:");
+// int[,] arr = new int[rnd.Next(3, 10), rnd.Next(5, 10)];
+// Console.WriteLine($"- с количеством строк: {arr.GetLength(0)}");
+// Console.WriteLine($"- с количеством столбцов: {arr.GetLength(1)}");
+// FillArrayRandom(arr);
+// PrintArr(arr);
+// Console.Write("Введите координаты искомого числа через запятую (строка, столбец): ");
+// int[] coordinate = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+// int k = coordinate[0]-1;
+// int l = coordinate[1]-1;
+
+// if (k < 0 | k >= arr.GetLength(0) | l < 0 | l >= arr.GetLength(1))    // проверка условия
+// {
+//     Console.WriteLine($"arr[{k}, {l}] -> такого значения нет!");
+// }
+// else
+// {
+//     Console.WriteLine($"arr[{k}, {l}] -> {arr[k, l]}");
+// }
+
+
+
+
+// void FillArrayRandom(int[,] arr)    // создаем рандомный двумерный массив из целых чисел
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = rnd.Next(0, 10);
+
+//         }
+
+//     }
+// }
+
+// void PrintArr(int[,] arry)  // метод вывода на печать массива
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arry[i, j],2}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
--- task052/Program.cs
+++ task052/Program.cs
@@ -9,49 +9,56 @@
 
 
 Random rnd = new Random();
-int m = rnd.Next(1, 5);
-Console.WriteLine($"Строк будет: {m}");
-int n = rnd.Next(1, 10);
-Console.WriteLine($"Столбцов будет: {n}");
+Console.WriteLine("Например, задан массив:");
+int m = rnd.Next(2, 5);
+Console.WriteLine($"- с количеством строк: {m}");
+int n = rnd.Next(2, 5);
+Console.WriteLine($"- с количеством столбцов: {n}");
 
-double[,] arr = new double[m, n];
+int[,] arr = new int[m, n];
 
-for (int i = 0; i < arr.GetLength(0); i++)
+FillArrayRandom(arr);
+PrintArr(arr);
+Console.Write("Среднее арифметическое каждого столбца: ");
+
+for (int j = 0; j < arr.GetLength(1); j++)  // условие при котором будет просчитываться среднее арифметическое
 {
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        sum = sum + arr[i, j];  // складываем значения столбца, т.к. проводим цикл под значением cтрок (0=m)
+    }
+    sum = Math.Round((sum / arr.GetLength(0)), 1);  // делим полученную сумму всех чисел столбца на кол-во этих чисел с округлением результата до десятых
+    Console.Write($" {sum}; ");
+}
+Console.Write("\b\b."); // меняем последнюю ; на .
+
+
+
+void FillArrayRandom(int[,] arr)    // создаем рандомный двумерный массив из целых чисел
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
         {
-            arr[i, j] = Math.Round((rnd.NextDouble() * 100 ), 0);
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = rnd.Next(0, 10);
+            }
         }
     }
 }
 
-PrintArr(arr);
-
-
-
-void PrintArr(double[,] arry)  // метод вывода на печать массива
+void PrintArr(int[,] arry)  // метод вывода на печать массива
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write($"{arry[i, j],8}");
+            Console.Write($"{arry[i, j],2}");
         }
         Console.WriteLine();
     }
 }
 
-Console.WriteLine();
 
 
-for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {double sum=0;
-            if (i  == 0 )
-            {
-                sum =arr[i, j] + arr[i+1, j]+ arr[i+2, j];
-            }Console.WriteLine(sum);
-        }
-    }
